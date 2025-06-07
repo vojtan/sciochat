b7b328e9e550e0efd035c72651a70ba66c50046d
Vojtech Nadvornik <vojtech.nadvornik@communardo.de>
2025-06-07 16:18:24 +0200

    added support of typescript

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <main>
+        Hello world
+      </main>
+    </div>
+  );
+}
+
+export default App;
